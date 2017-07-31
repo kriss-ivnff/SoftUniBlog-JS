fdb5abf1da853fa6d0f7fe88dcd151e3db7c70b5
kriss-ivnff <super_gravity@abv.bg>
2017-07-31 18:37:27 +0300

    Blog

--- config/routes.js
+++ config/routes.js
@@ -16,5 +16,7 @@ module.exports = (app) => {
     app.get('/article/create', articleController.createGet());
     app.post('/article/create', articleController.createPost());
 
+    app.get('/article/details/:id', articleController.details);
+
 };
 
